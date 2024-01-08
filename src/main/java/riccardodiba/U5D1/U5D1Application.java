1ed0bf6a8e49e47a5ebea78a8c087a0f9d9cc690
Riccardo Di Bari <riccardodibari@hotmail.it>
2024-01-08 15:44:56 +0100

    configurazione

---
+++ src/main/java/riccardodiba/U5D1/U5D1Application.java
@@ -0,0 +1,13 @@
+package riccardodiba.U5D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/riccardodiba/U5D1/U5D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package riccardodiba.U5D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
